5243af1c948ea91319d5c8bd42234bc92a937e0d
Woojin Lee <dev.woojin.lee@gmail.com>
2025-05-19 21:11:16 +0900

    feature: add contentlayer

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import { withContentlayer } from "next-contentlayer";
 
 const nextConfig: NextConfig = {
   output: "export",
@@ -7,4 +8,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withContentlayer(nextConfig);
