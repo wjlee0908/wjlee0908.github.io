ad5457da67407c71febcf7fbaea9983daa52a1f2
Woojin Lee <dev.woojin.lee@gmail.com>
2025-05-20 21:56:28 +0900

    feature: add title

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "이우진 기술 블로그",
+  url: "https://wjlee0908.github.io/",
+  links: {
+    github: "https://github.com/wjlee0908",
+  },
+};
