602ae2cc5803cf6334b86e8d6af5a9ed952042bd
Woojin Lee <dev.woojin.lee@gmail.com>
2025-05-21 20:59:00 +0900

    feature: modify root page styles

--- out/_next/static/chunks/app/page-71fad192d3edb16a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{6400:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6378,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[378,309,951,358],()=>s(6400)),_N_E=e.O()}]);
