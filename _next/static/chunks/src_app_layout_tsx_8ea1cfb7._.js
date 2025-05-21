f768a85ccf451ea57d3a7ba888440724b25029cb
Woojin Lee <dev.woojin.lee@gmail.com>
2025-05-21 20:59:24 +0900

    Updates

--- _next/static/chunks/src_app_layout_tsx_8ea1cfb7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_76c15e8b._.css",
-  "static/chunks/30cb0_next_dist_55f666e1._.js"
-],
-    source: "dynamic"
-});
